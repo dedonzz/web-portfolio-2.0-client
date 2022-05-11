5bbccb8019124a07d36f8208bf8e8ef7ca34ce10
Don <donwcj@gmail.com>
2022-05-11 18:40:14 +0800

    Modified Theme.jsx

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Bounce from "react-reveal/Bounce";
 import "./styles/Theme.css";
 
